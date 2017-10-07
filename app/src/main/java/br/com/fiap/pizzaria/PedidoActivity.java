7c2f4f457ac6c42968303395e82d6b5e869d7c52
logonrm <logonrm@fiap.com.br>
2017-10-07 10:55:46 -0300

    Criaca da tela de pedidos 2

--- app/src/main/java/br/com/fiap/pizzaria/PedidoActivity.java
+++ app/src/main/java/br/com/fiap/pizzaria/PedidoActivity.java
@@ -2,12 +2,23 @@ package br.com.fiap.pizzaria;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class PedidoActivity extends AppCompatActivity {
+
+    @BindView(R.id.cbAtum)
+    CheckBox cbAtum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedido);
+
+        ButterKnife.bind(this);
+        cbAtum.isChecked();
+
     }
 }
